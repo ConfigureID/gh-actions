a620a04ca9c9a8b122437e9cc98cf073dd7430fa
Martin Moscovich <martinmoscovich@gmail.com>
2022-11-11 15:47:45 -0300

    rsync and cache (#3)
    Co-authored-by: Elvis Onate <elvis@flugel.it>
    Co-authored-by: elvisfgl <102542543+elvisfgl@users.noreply.github.com>
    Co-authored-by: Martin Moscovich <martinm@createme.com>

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -26,14 +26,12 @@ fi
 if [[ $INPUT_TO =~ $INPUT_ALLOWED_DESTINATION ]]; then
     echo "Destination allowed"
 else
-    echo "Error - Destination not allowed ($INPUT_TO)"
-    exit 1
+    echo "::error::Error - Destination not allowed ($INPUT_TO)" && exit 1
 fi
 
 # Validate the directory is not empty
 if [ -z "$(ls -A ${INPUT_PATH})" ]; then
-    echo "Error - The source directory is empty. Publish not allowed"
-    exit 1
+    echo "::error::Error - The source directory is empty. Publish not allowed" && exit 1
 else
     echo "The source directory contains files. Publish allowed"
 
@@ -42,8 +40,7 @@ else
     echo gsutil -m $CACHE_OPTIONS rsync -r -c $DELETE_OPTION -x '$INPUT_EXCLUDE' /github/workspace/$INPUT_PATH gs://$INPUT_CLOUD_BUCKET/$INPUT_TO | bash
 
     if [ $? -ne 0 ]; then
-        echo "Syncing failed"
-        exit 1
+        echo "::error::Syncing failed" && exit 1
     fi
     echo "Done."
 fi
